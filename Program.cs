078a3676beda4555ec3bf893cf3c1acf90c6ef7f
Arunangshu Biswas <arunangshu.biswas.microlise@gmail.com>
2022-05-11 10:59:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace WorkOut
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int r1 = 80, r2 = 12, r3 = 120, r4 = 219;
+            int ans = r4 | r3 << 8;
+            int r3LeftShift8 = r3 << 8;
+            int ans2 = r4 | r3LeftShift8;
+            Console.WriteLine(Convert.ToString(r3, 2) + "        " + r3LeftShift8 + "        " + Convert.ToString(r3LeftShift8, 2));
+            Console.WriteLine(ans + "   " + ans2);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
