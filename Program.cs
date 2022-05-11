725b96253b10e171c425f31bb2cfb53d15403d56
Arunangshu31 <103297509+Arunangshu31@users.noreply.github.com>
2022-05-11 11:05:26 +0530

    added unwanted comments

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@
             Console.WriteLine(ans + "   " + ans2);
             Console.WriteLine();
             Console.ReadLine();
+            //creating merge conflict ha ha ha
         }
     }
 }
