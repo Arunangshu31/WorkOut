92c2ea6e641376602e929d4d4d2aaa442345b6b0
Arunangshu31 <103297509+Arunangshu31@users.noreply.github.com>
2022-05-11 11:02:06 +0530

    added code comments

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@
 {
     class Program
     {
+        
         public static void Main()
         {
+            // code for confirmance of operator precedence and left shift operator
             int r1 = 80, r2 = 12, r3 = 120, r4 = 219;
             int ans = r4 | r3 << 8;
             int r3LeftShift8 = r3 << 8;
